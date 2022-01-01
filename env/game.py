a1baf56434a7e25e9bab64556d2473075cd4c4bd
Onii Chan <46487694+sogood99@users.noreply.github.com>
2022-01-01 17:00:03 +0800

    Update game.py

--- env/game.py
+++ env/game.py
@@ -5,14 +5,13 @@ import numpy as np
 from stable_baselines.common.vec_env import DummyVecEnv
 from .env import DoomEnv, DoomWithBots
 from .game_actions import get_available_actions
-from .config import EnvironmentConfig
 
 
 def init_game(scenario, show_window=False):
     game = DoomGame()
     game.set_window_visible(show_window)
     game.load_config(f'../scenarios/{scenario}.cfg')
-    game.set_window_visible(True)
+    game.set_window_visible(False)
     game.add_game_args('-host 1 -deathmatch +viz_nocheat 0 +cl_run 1 +name ANO +colorset 0' +
                        '+sv_forcerespawn 1 +sv_respawnprotect 1 +sv_nocrouch 1 +sv_noexit 1')
     game.init()
